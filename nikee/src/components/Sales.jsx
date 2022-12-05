09a7681b4f7cf6be48f9458623f7933b6e6d45f0
Samuel <samueltwumasi1290.com>
2022-12-05 00:00:49 +0000

    update

--- nikee/src/components/Sales.jsx
+++ nikee/src/components/Sales.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Item from "./utils/Item";
 import Title from "./utils/Title";
 
-function Sales({endpoint: {title, items}}) {
+function Sales({ifExists, endpoint: {title, items}}) {
   return (
     <>
-      <div>
+      <div className="nike-container transition-all duration-500">
         <Title title={title} />
-        <div>
+        <div className={`gap-7 grid lg:gap-5 mt-7 items-center justify-items-center ${ifExists ? 'grid-cols-3 xl:grid-cols-2 sm:grid-cols-1' : 'grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 grid-cols-4'}`}>
             {items?.map((val, i) => (
-                <Item key={i} {...val} />
+                <Item key={i} {...val} ifExists={ifExists} />
             ))}
         </div>
       </div>
