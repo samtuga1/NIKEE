3067184a909be420f1c84f61d8b5af7414ae75d1
Samuel <samueltwumasi1290.com>
2022-12-11 10:31:46 +0000

    update

--- nikee/src/App.jsx
+++ nikee/src/App.jsx
@@ -1,19 +1,19 @@
-import './App.css';
-import { Hero, Sales } from './components';
-import {heroapi, popularsales, toprateslaes} from './data/data.js';
+import "./App.css";
+import { Hero, Sales } from "./components";
+import FlexContent from "./components/FlexContent";
+import { heroapi, popularsales, toprateslaes } from "./data/data.js";
 
 function App() {
-
   return (
-   <>
-   <main className='flex flex-col relative gap-16' >
-    <Hero heroapi={heroapi} />
-   <Sales endpoint={popularsales} ifExists />
-   <Sales endpoint={toprateslaes} />
-   </main>
-   
-   </>
-  )
+    <>
+      <main className="flex flex-col relative gap-16">
+        <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} ifExists />
+        <Sales endpoint={toprateslaes} />
+        <FlexContent />
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
