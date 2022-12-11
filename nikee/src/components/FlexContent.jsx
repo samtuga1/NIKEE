8b38d1c2bdfd1735bda58a70efa1e775d83c924f
Samuel <samueltwumasi1290.com>
2022-12-11 10:46:04 +0000

    update

--- nikee/src/components/FlexContent.jsx
+++ nikee/src/components/FlexContent.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-function FlexContent() {
-  return <div>FlexContent</div>;
+function FlexContent({ endpoint: { title, heading, img, text, btn, url } }) {
+  return (
+    <>
+      <div className="">
+        <div>
+          <h1>{heading}</h1>
+          <h1>{title}</h1>
+          <p>{text}</p>
+          <a href={url}>
+            <button type="button" className="">
+              {btn}
+            </button>
+          </a>
+        </div>
+
+        <div>
+          <img src={img} img={`img/${heading}`} className="" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default FlexContent;
