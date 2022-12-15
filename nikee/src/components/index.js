e8d4b2d3d8d89544d58e93754277670c76be52ce
Samuel <samueltwumasi1290.com>
2022-12-15 08:22:01 +0000

    update

--- nikee/src/components/index.js
+++ nikee/src/components/index.js
@@ -1,3 +1,6 @@
 export {default as Hero} from './Hero.jsx';
 export {default as Sales} from './Sales.jsx';
 export {default as Stories} from './Stories';
+export {default as Footer} from './Footer';
+export {default as Navbar} from './Navbar';
+export {default as Cart} from './Cart';
